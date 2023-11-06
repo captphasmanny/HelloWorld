2c8c7e98c7a5376000241a4f9bc99b3ff9d5217e
Manny <captphasmanny@att.net>
2023-11-06 08:42:41 -0500

    Added note regarding Main functions.
    Changed "Earth" back to "World"

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <limits>
 
-int main()
+int main() //ALL PROGRAMS MUST INCLUDE A MAIN FUNCTION
 {
-    std::cout << "Hello, Earth!";
+    std::cout << "Hello, World!" << std::endl;
     std::cin.clear(); //resets any error flags
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //ignore any characters in the input buffer until we find a new line
     std::cin.get(); //get one more character from the user (waits for user to press enter)
