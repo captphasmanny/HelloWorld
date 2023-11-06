dd879993def59fcdd466d74857cce66ed954f501
Manny <captphasmanny@att.net>
2023-11-06 08:27:48 -0500

    I changed "World" to "Earth"

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-    std::cout << "Hello, World!";
+    std::cout << "Hello, Earth!";
     std::cin.clear(); //resets any error flags
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //ignore any characters in the input buffer until we find a new line
     std::cin.get(); //get one more character from the user (waits for user to press enter)
