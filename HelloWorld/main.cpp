5d327092d63f0a6d9f4849c0b7977d78f371cdbf
Manny <captphasmanny@att.net>
2023-11-06 08:11:29 -0500

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    std::cout << "Hello, World!";
+    std::cin.clear(); //resets any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //ignore any characters in the input buffer until we find a new line
+    std::cin.get(); //get one more character from the user (waits for user to press enter)
+    return 0;
+}
